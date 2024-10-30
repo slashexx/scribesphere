5c1235fca984ab2428673a24d1eba78dfc00d847
slashexx <dhruvpuri.35@gmail.com>
2024-10-30 23:00:02 +0530

    Fix tests

--- scribesphere/src/tests/App.test.tsx
+++ scribesphere/src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 // src/App.test.js
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 test("renders the home page", () => {
   render(<App />);
